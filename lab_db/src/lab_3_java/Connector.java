e1c4bffa285d6806714824cd181767c78f6523b6
Anton <anton.komar2979@gmail.com>
2017-12-16 02:30:39 +0200

    Hide accept

--- lab_db/src/lab_3_java/Connector.java
+++ lab_db/src/lab_3_java/Connector.java
@@ -1,8 +1,11 @@
 package lab_3_java;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.sql.*;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class Connector {
 
@@ -12,26 +15,26 @@ public class Connector {
         Connector.connector();
     }
     public static void connector() {
-        String url = "jdbc:postgresql://217.23.154.1:5432/labdb";
-        String userName = "antkom";
-        String password = "antonkomaria61";
-//        String s = "";
-//        Scanner in = null;
-//
-//        try {
-//            in = new Scanner(new File("lab_db/src/signIn.txt"));
-//        } catch (FileNotFoundException e) {
-//            e.printStackTrace();
-//        }
-//        while(in.hasNext()) {
-//            s += in.nextLine();
-//        }
-//        in.close();
-//
-//        String[] data = s.split(" ");
-//        url = data[0];
-//        userName = data[1];
-//        password = data[2];
+        String url;
+        String userName;
+        String password;
+                String s = "";
+        Scanner in = null;
+
+        try {
+            in = new Scanner(new File("lab_db/src/signIn.txt"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        while(in.hasNext()) {
+            s += in.nextLine();
+        }
+        in.close();
+
+        String[] data = s.split(" ");
+        url = data[0];
+        userName = data[1];
+        password = data[2];
 
            try {
                Class.forName("org.postgresql.Driver");
